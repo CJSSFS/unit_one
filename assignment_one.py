5f4fb62a9ada702e55925fdedb0110f829c905e5
CJ <cj@iMac7.local>
2018-09-14 14:01:08 -0400

    Finished creating all four octagons with color

--- assignment_one.py
+++ assignment_one.py
@@ -1,4 +1,11 @@
+# Chad Scott
+# 9/13/18
+# assignment_one.py
+# This is the first option to draw four differently colored octagons
+
+
 import turtle
+turtle.speed(50)
 def draw_an_octagon():
 
 
@@ -11,7 +18,7 @@ turtle.color("orange")
 turtle.begin_fill()
 draw_an_octagon()
 turtle.end_fill()
-
+# I am telling the turtle to go to the next point and draw the next octagon
 turtle.up()
 turtle.forward(200)
 turtle.down()
@@ -22,8 +29,9 @@ draw_an_octagon()
 turtle.end_fill()
 
 turtle.up()
-turtle.left(90)
-turtle.forward(250)
+turtle.back(250)
+turtle.right(90)
+turtle.forward(100)
 turtle.down()
 
 turtle.color("red")
@@ -31,6 +39,17 @@ turtle.begin_fill()
 draw_an_octagon()
 turtle.end_fill()
 
+turtle.up()
+turtle.left(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.down()
+
+
+turtle.color("yellow")
+turtle.begin_fill()
+draw_an_octagon()
+turtle.end_fill()
 
 turtle.exitonclick()
 
